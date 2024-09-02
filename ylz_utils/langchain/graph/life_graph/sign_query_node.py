901557f88c5f2c5207e2ebffc2040e5b7368e501
youht <youht@youhtdeMac-mini.local>
2024-09-02 19:10:26 +0800

    fix

--- ylz_utils/langchain/graph/life_graph/sign_query_node.py
+++ ylz_utils/langchain/graph/life_graph/sign_query_node.py
@@ -1,3 +1,4 @@
+import datetime
 from  .state import Signs,State
 from .node import Node
 
@@ -10,28 +11,29 @@ class SignQueryNode(Node):
         message = state["messages"][-1]
         prompt_template = \
         """"
+        现在时间是:{now}
         根据neo4j的schema和一步一步生成查询句子，确保生成neo4j查询语句script与查询的目标相关，且可以正确执行。
         要求1、仅输出script，不要做任何解释
            2、仅包含相关的节点和关系
-           3、不要使用neo4j不存在的时间函数
+           3、不要使用neo4j不存在的时间函数,使用date(sdt),date(edt)返回日期
         schema:{schema}
         """
         prompt = self.lifeGraph.graphLib.langchainLib.get_prompt(prompt_template)
         subTag = state["life_tag"].subTags[0]
         state["life_tag"].subTags = state["life_tag"].subTags[1:]
         schema_description = """
-           (:Person).properties: {{name:姓名[String]}}
-           (:Sign).properties: {{name:体征的名称[String]}}
-           (:Place).properties: {{name:地点名称[String]}}
-           [sign].properties: {{sdt:起始时间[例如:2024-09-02T09:32:32.491831000],edt:截止时间[例如:2024-09-02T09:32:32.491831000],place:测量地点[String],value:测量的数据[Double],unit:测量的数据的单位[String],buy:测量所花费的金额[Double],duration:测量所花费的时间[String]}}
-           [at].properties:{{sdt:起始时间[例如:2024-09-02T09:32:32.491831000],edt:截止时间[例如:2024-09-02T09:32:32.491831000]}}
-           (:Person)-[:sign]->(:Sign) 某人测量某项身体指标
+           (:Person).properties: <<name:姓名[String]>>
+           (:Sign).properties: <<name:体征的名称[String]>>
+           (:Place).properties: <<name:地点名称[String]>>
+           [s:sign].properties: <<sdt:起始时间,edt:截止时间,place:测量地点[String],value:测量的数据[Double],unit:测量的数据的单位[String],buy:测量所花费的金额[Double],duration:测量所花费的时间[String]>>
+           [at].properties:<<sdt:起始时间,edt:截止时间>>
+           (:Person)-[:sign{{sdt,edt,place,value,unit,buy,duration}}]->(:Sign) 某人测量某项身体指标
            (:Person)-[:at]->(:Place) 某人在某地
         """
         rel_schema = filter(lambda x:x.get("relType") in [":`sign`",":`at`"],neo4jLib.get_node_schema())
         node_schema = filter(lambda x:x.get("nodeType") in [":`Person`",":`Sign`",":`Place`"],neo4jLib.get_node_schema())
         schema = f"schema description:\n{schema_description}\nrelationship schema:\n{rel_schema}\nnode schema:\n{node_schema}\n"
-        res = (prompt | llm).invoke({"schema":schema,"input":subTag.sub_text})
+        res = (prompt | llm).invoke({"now":datetime.datetime.now(),"schema":schema,"input":subTag.sub_text})
 
         print("query script---->",res)
         # if isinstance(res,Signs):
