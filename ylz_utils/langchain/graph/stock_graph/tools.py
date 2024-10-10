78b41f28727aab31a8b9e87a8bbfc3126e5cd286
youht <youht@youhtdeMac-mini.local>
2024-10-10 18:53:49 +0800

    update stock

--- ylz_utils/langchain/graph/stock_graph/tools.py
+++ ylz_utils/langchain/graph/stock_graph/tools.py
@@ -1,3 +1,4 @@
+from typing import Literal
 import pysnowball as ball
 import tushare
 import pandas as pd
@@ -6,50 +7,9 @@ import json
 import os
 from ylz_utils.config import Config 
 
-from pydantic import BaseModel, Field
-from datetime import datetime
 from rich import print
 
-class CompanyInfo(BaseModel):
-    name: str = Field(..., description="公司名称")
-    ename: str = Field(..., description="公司英文名称")
-    market: str = Field(..., description="上市市场")
-    idea: str = Field(..., description="概念及板块，多个概念由英文逗号分隔")
-    ldate: datetime = Field(..., description="上市日期，格式yyyy-MM-dd")
-    sprice: str = Field(..., description="发行价格（元）")
-    principal: str = Field(..., description="主承销商")
-    rdate: str = Field(..., description="成立日期")
-    rprice: str = Field(..., description="注册资本")
-    instype: str = Field(..., description="机构类型")
-    organ: str = Field(..., description="组织形式")
-    secre: str = Field(..., description="董事会秘书")
-    phone: str = Field(..., description="公司电话")
-    sphone: str = Field(..., description="董秘电话")
-    fax: str = Field(..., description="公司传真")
-    sfax: str = Field(..., description="董秘传真")
-    email: str = Field(..., description="公司电子邮箱")
-    semail: str = Field(..., description="董秘电子邮箱")
-    site: str = Field(..., description="公司网站")
-    post: str = Field(..., description="邮政编码")
-    infosite: str = Field(..., description="信息披露网址")
-    oname: str = Field(..., description="证券简称更名历史")
-    addr: str = Field(..., description="注册地址")
-    oaddr: str = Field(..., description="办公地址")
-    desc: str = Field(..., description="公司简介")
-    bscope: str = Field(..., description="经营范围")
-    printype: str = Field(..., description="承销方式")
-    referrer: str = Field(..., description="上市推荐人")
-    putype: str = Field(..., description="发行方式")
-    pe: str = Field(..., description="发行市盈率（按发行后总股本）")
-    firgu: str = Field(..., description="首发前总股本（万股）")
-    lastgu: str = Field(..., description="首发后总股本（万股）")
-    realgu: str = Field(..., description="实际发行量（万股）")
-    planm: str = Field(..., description="预计募集资金（万元）")
-    realm: str = Field(..., description="实际募集资金合计（万元）")
-    pubfee: str = Field(..., description="发行费用总额（万元）")
-    collect: str = Field(..., description="募集资金净额（万元）")
-    signfee: str = Field(..., description="承销费用（万元）")
-    pdate: datetime = Field(..., description="招股公告日")
+from ylz_utils.langchain.graph.stock_graph.state import *
 
 class StockTools:
     stock:list = []
@@ -58,25 +18,415 @@ class StockTools:
         
         # 获取当前模块的目录
         current_dir = os.path.dirname(os.path.abspath(__file__))
-        # 构建 JSON 文件的完整路径
-        stock_file = os.path.join(current_dir, 'stock.json')
-        with open(stock_file, 'r', encoding='utf-8') as f:
-            self.stock = json.load(f)
-
+        # 构建 gpdm 文件的完整路径
+        gpdm_file = os.path.join(current_dir, 'gpdm.json')
+        with open(gpdm_file, 'r', encoding='utf-8') as f:
+            self.gpdm = json.load(f)
+         # 构建 zsdm 文件的完整路径
+        zsdm_file = os.path.join(current_dir, 'zsdm.json')
+        with open(zsdm_file, 'r', encoding='utf-8') as f:
+            self.zsdm = json.load(f)
+    def _get_stock_code(self,stock_name:str):
+        """根据股票或指数名称获取股票/指数代码"""
+        stock_info = list(filter(lambda item:item["name"]==stock_name.upper(),self.gpdm))
+        if stock_info:
+            stock_code = stock_info[0]['symbol']
+            return stock_code
+        else:
+            zhishu_info = list(filter(lambda item:item["mc"]==stock_name.upper(),self.zsdm))
+            if zhishu_info:
+                zhishu_code = zhishu_info[0]['dm']
+                return zhishu_code
+        return stock_name
+        
 class MairuiTools(StockTools):
     def __init__(self,graphLib):
         super().__init__(graphLib)
         self.mairui_token = Config.get('STOCK.MAIRUI.TOKEN')
-        self.mairui_api_url = "http://api.mairui.club"
-        
-    def get_company_info(self, code:str)->CompanyInfo:
-        """获取公司基本信息"""
+        self.mairui_api_url = "http://api.mairui.club" 
+    def _export(self):
+        return [
+            'get_hscp_cwzb','get_hscp_jdlr','get_hscp_jdxx',
+            'get_hsmy_jddxt','get_hsmy_lscjt','get_hsrl_zbdd'
+        ]       
+    def get_hscp_gsjj(self, code:str)->CompanyInfo:
+        """获取公司基本信息和IPO基本信息"""
+        code = self._get_stock_code(code)
         res = requests.get( 
             f"{self.mairui_api_url}/hscp/gsjj/{code}/{self.mairui_token}",
         )
         data = res.json()        
         return CompanyInfo(**data)
     
+    def get_hscp_sszs(self, code:str):
+        """获取公司所属的指数代码和名称"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/sszs/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_ljgg(self, code:str):
+        """获取公司历届高管成员名单"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/ljgg/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_ljds(self, code:str):
+        """获取公司历届董事成员名单"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/ljds/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_ljjj(self, code:str):
+        """获取公司历届监事成员名单"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/ljjj/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_jdlr(self, code:str):
+        """获取公司近一年各季度利润"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/jdlr/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_jdxj(self, code:str):
+        """获取公司近一年各季度现金流"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/jdxj/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_cwzb(self, code:str)->list[FinancialReport]:
+        """获取公司近一年各季度主要财务指标"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/cwzb/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return [FinancialReport(**item) for item in data]
+        #return list(map(lambda item:FinancialReport(**item),data))
+    def get_hscp_sdgd(self, code:str):
+        """获取公司十大股东"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/sdgd/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_ltgd(self, code:str):
+        """获取公司十大流通股股东"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/ltgd/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_gdbh(self, code:str):
+        """获取公司股东变化趋势"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/gdbh/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hscp_jjcg(self, code:str):
+        """获取公司最近500家左右的基金持股情况"""
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hscp/jjcg/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsmy_zlzj(self,code:str):
+        """获取某个股票的每分钟主力资金走势"""
+        #数据更新：每天20:00开始更新（更新耗时约4小时）
+        #请求频率：1分钟300次 
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsmy/zlzh/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsmy_zjlr(self,code:str):
+        """获取某个股票的近十年每天资金流入趋势"""
+        #数据更新：每天20:00开始更新（更新耗时约4小时）
+        #请求频率：1分钟300次 
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsmy/zjlr/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsmy_zhlrt(self,code:str):
+        """获取某个股票的近10天资金流入趋势"""
+        #数据更新：每天20:00开始更新（更新耗时约4小时）
+        #请求频率：1分钟300次 
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsmy/zhlrt/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsmy_jddx(self,code:str):
+        """获取某个股票的近十年主力阶段资金动向"""
+        #数据更新：每天20:00开始更新（更新耗时约4小时）
+        #请求频率：1分钟300次 
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsmy/jddx/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsmy_jddxt(self,code:str):
+        """获取某个股票的近十天主力阶段资金动向"""
+        #数据更新：每天20:00开始更新（更新耗时约4小时）
+        #请求频率：1分钟300次 
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsmy/jddxt/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsmy_lscj(self,code:str):
+        """获取某个股票的近十年每天历史成交分布"""
+        #数据更新：每天20:00开始更新（更新耗时约4小时）
+        #请求频率：1分钟300次 
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsmy/lscj/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsmy_lscjt(self,code:str):
+        """获取某个股票的近十天历史成交分布"""
+        #数据更新：每天20:00开始更新（更新耗时约4小时）
+        #请求频率：1分钟300次 
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsmy/lscjt/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsrl_ssjy(self,code:str):
+        """获取某个股票的实时交易数据"""
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsrl/ssjy/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsrl_mmwp(self,code:str):
+        """获取某个股票的盘口交易数据"""
+        #数据更新：交易时间段每2分钟
+        #请求频率：1分钟300次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsrl/mmwp/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsrl_zbjy(self,code:str):
+        """获取某个股票的当天逐笔交易数据"""
+        #数据更新：每天20:00开始更新，当日23:59前完成
+        #请求频率：1分钟300次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsrl/zbjy/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsrl_fscj(self,code:str):
+        """获取某个股票的当天分时成交数据"""
+        #数据更新：每天20:00开始更新，当日23:59前完成
+        #请求频率：1分钟300次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsrl/fscj/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsrl_fjcj(self,code:str):
+        """获取某个股票的当天分价成交数据"""
+        #数据更新：每天20:00开始更新，当日23:59前完成
+        #请求频率：1分钟300次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsrl/fjcj/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hsrl_zbdd(self,code:str):
+        """获取某个股票的当天逐笔超400手的大单成交数据"""
+        #数据更新：每天20:00开始更新，当日23:59前完成
+        #请求频率：1分钟300次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hsrl/zbdd/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_hszb_fsjy(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取股票代码分时交易实时数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hszb/fsjy/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_hszb_ma(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取股票代码分时交易的平均移动线数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hszb/ma/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    
+    def get_hszbl_fsjy(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取股票代码分时交易历史数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hszbl/fsjy/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_hszbl_ma(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取股票代码分时交易的平均移动线历史数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hszbl/ma/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    
+    def get_hszbc_fsjy(self,code:str,sdt:str,edt:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取股票代码某段时间分时交易历史数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hszbc/fsjy/{code}/{fs}/{sdt}/{edt}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_hszbc_ma(self,code:str,sdt:str,edt:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取股票代码某段时间分时交易的平均移动线历史数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/hszbc/ma/{code}/{fs}/{sdt}/{edt}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_zs_sssj(self,code:str):
+        """获取某个指数的实时交易数据"""
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/zs/sssj/{code}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    
+    def get_zs_lsgl(self):
+        """获取沪深两市不同涨跌幅的股票数统计"""
+        #数据更新：交易时间段每2分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        res = requests.get( 
+            f"{self.mairui_api_url}/zs/lsgl/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_zs_fsjy(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取指数代码分时交易实时数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/zs/fsjy/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_zs_ma(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取指数代码分时交易的平均移动线数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/zs/ma/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    
+    def get_zs_hfsjy(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取指数代码分时交易历史数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/zs/hfsjy/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_zs_hma(self,code:str,fs:Literal["5m","15m","30m","60m","dn","wn","mn","yn"]="5m"):
+        """获取指数代码分时交易的平均移动线历史数据。分时级别支持5分钟、15分钟、30分钟、60分钟、日周月年级别，对应的值分别是 5m、15m、30m、60m、dn、wn、mn、yn """
+        #数据更新：交易时间段每1分钟
+        #请求频率：1分钟600次 | 包年版1分钟3千次 | 钻石版1分钟6千次
+        code = self._get_stock_code(code)
+        res = requests.get( 
+            f"{self.mairui_api_url}/zs/hma/{code}/{fs}/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+
+    def get_hitc_jrts(self):
+        """获取今日股票、基金公告事项以及交易异动概览"""
+        #数据更新：每天15:30（约10分钟更新完成）
+        #请求频率：1分钟20次
+        res = requests.get( 
+            f"{self.mairui_api_url}/hitc/jrts/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
+    def get_hitc_dzjy(self):
+        """获取上一个交易日的大宗交易数据"""
+        #数据更新：每天15:30（约10分钟更新完成）
+        #请求频率：1分钟20次
+        res = requests.get( 
+            f"{self.mairui_api_url}/hitc/dzjy/{self.mairui_token}",
+        )
+        data = res.json()        
+        return data
 class TushareTools(StockTools):
     def __init__(self,graphLib):
         super().__init__(graphLib)
@@ -114,14 +464,6 @@ class SnowballTools(StockTools):
         ball.set_token(f"xq_a_token={snowball_token};") 
         print(f"snowball token:{snowball_token}")
 
-    def _get_stock_code(self,stock_name):
-        """根据股票名称获取股票代码"""
-        stock_info = list(filter(lambda item:item["name"]==stock_name,self.stock))
-        if stock_info:
-            stock_code = stock_info[0]['symbol']
-            return stock_code
-        else:
-            return ""
     def _ball_code(self,code:str):
         if not code.isnumeric():
             code = self._get_stock_code(code)
@@ -261,5 +603,7 @@ if __name__ == "__main__":
     # toolLib = SnowballTools(stockGraph)
     # data  = toolLib.balance("ST易联众")
     toolLib = MairuiTools(stockGraph)
-    data = toolLib.get_company_info("300096")
+    #data = toolLib.get_company_info("ST易联众")
+    data = toolLib.get_hscp_cwzb("ST易联众")
     print(data)
+    print(len(data))
